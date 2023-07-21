b83d6f989a14def3e9812bc99c57e54d1ce21b41
Jesus Borjas <jesusbor006@gmail.com>
2023-07-21 17:20:39 -0500

    Add back navigation for better user experience

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,20 +4,21 @@ import { Link } from "react-router-dom";
 const Navbar = (props) => {
   return (
     <nav className="navbar">
-      <h1 className="logo">FitQuest</h1>
+      <Link to="/home">
+        <h1 className="logo">FitQuest</h1>
+      </Link>
       <div className="nav-right">
-      <Link to="/home" className="home-link">
-        Home
-      </Link>
-      <Link to="/favorites" className="favorites-link">
-        Favorites
-      </Link>
-      <Avatar
-        sx={{ cursor: "pointer", width: "50px", height: "50px" }}
-        alt={props.activeUser.name}
-        src={props.activeUser.image}
-      />
-
+        <Link to="/home" className="home-link">
+          Home
+        </Link>
+        <Link to="/favorites" className="favorites-link">
+          Favorites
+        </Link>
+        <Avatar
+          sx={{ cursor: "pointer", width: "50px", height: "50px" }}
+          alt={props.activeUser.name}
+          src={props.activeUser.image}
+        />
       </div>
     </nav>
   );
--- frontend/src/pages/Workouts.js
+++ frontend/src/pages/Workouts.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import ExercisesContainer from "../components/ExercisesContainer";
 import Navbar from "../components/Navbar";
 
@@ -5,6 +6,7 @@ const Workouts = (props) => {
   return (
     <div className="workout-page">
       <Navbar activeUser={props.activeUser} />
+      <Link to="/home" className="back-link"> ⬅ Go Back</Link>
       <h2 className="workout-heading">{props.activeMuscle}</h2>
       <ExercisesContainer
         filteredExercises={props.filteredExercises}
